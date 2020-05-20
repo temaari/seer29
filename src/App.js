07f37db11b938de11a7598e02e9b9ea6d0199782
temaari <53286793+temaari@users.noreply.github.com>
2020-05-20 17:44:15 +1200

    adding some simple input areas

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ export class App extends React.Component {
  
   render() {
     return (
-      <div className="App-header">
-        Hello there this is some random page
+      <div className="App">
+        <header className="App-header">This is the app</header>
+        <div>
+        <p>
+          Hello there this is some random page
+        </p>
+        <input></input>
+        <input type="Button" value="Click me"></input>
+        </div>
       </div>
     )
   }
