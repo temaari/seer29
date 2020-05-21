72dc58f0ec0a621b69de6def1319bac2006a6b81
temaari <53286793+temaari@users.noreply.github.com>
2020-05-22 11:58:44 +1200

    This is adding a table and search button

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ export class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">This is the app</header>
+        <header className="App-header">SEER</header>
         <div>
         <p>
-          Hello there this is some random page
+          Welcome to the SEER Search Page
         </p>
         <input></input>
-        <input type="Button" value="Click me"></input>
+        <input type="Button" value="Search"></input>
+        </div>
+        <div>
+          <table>
+            <th>
+              Name
+            </th>
+            <th>
+              Article
+            </th>
+            <th>
+              Authors
+            </th>
+          </table>
         </div>
       </div>
     )
