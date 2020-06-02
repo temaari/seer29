0f6c68c4e1d9910703112194d0236ae97d45810e
temaari <53286793+temaari@users.noreply.github.com>
2020-06-02 19:52:28 +1200

    update the articles component

--- client/src/App.js
+++ client/src/App.js
@@ -1,122 +1,19 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
+import {Articles} from './components/Articles.js';
 
 class App extends React.Component {
 
-  state = {
-    author: '',
-    title: '',
-    journal: '',
-    year: '',
-    month: '',
-    volume: '',
-    number: '',
-    pages: '',
-    eprint: '',
-    eprinttype: '',
-    eprintclass: '',
-    annote:'',
-    articles: []
-  };
-
-  componentDidMount = () => {
-    this.getArticles();
-  };
-
-  getArticles = () => {
-    axios.get('/api')
-      .then((response) => {
-        const data = response.data;
-        this.setState({ articles: data });
-        console.log('Data has been received!!');
-      })
-      .catch(() => {
-        console.log("error error!!!!")
-        // alert('Error retrieving data');//TO-DO:remove this later and deal with error
-      });
-  };
-
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  };
-
-  submit = (event) => {
-    event.preventDefault();
-
-    const payload = {
-      author: this.state.author,
-      title: this.state.title,
-      journal: this.state.journal,
-      year: this.state.year,
-      month: this.state.month,
-      volume: this.state.volume,
-      number: this.state.number,
-      pages: this.state.pages,
-      eprint: this.state.eprint,
-      eprinttype: this.state.eprinttype,
-      eprintclass: this.state.eprintclass,
-      annote: this.state.annote
-    };
-
-    axios({
-      url: '/api/save',
-      method: 'POST',
-      data: payload
-    })
-      .then(() => {
-        console.log('Data has been sent to the server');
-        this.resetUserInputs();
-        this.getArticles();
-      })
-      .catch(() => {
-        console.log('Internal server error');
-      });;
-  };
-
-  resetUserInputs = () => {
-    this.setState({
-      author: '',
-      title: '',
-      journal: '',
-      year: '',
-      month: '',
-      volume: '',
-      number: '',
-      pages: '',
-      eprint: '',
-      eprinttype: '',
-      eprintclass: '',
-      annote:''
-    });
-  };
-
-  displayArticles = (articles) => {
-    if (!articles.length) return null;
-
-    return articles.map((article, index) => (
-      <div key={index} className="articles_display">
-        <h3 className="">{article.author}</h3>
-        <p>{article.title}</p>
-      </div>
-    ));
-  };
-
   render() {
-
-    console.log('State', this.state);
 
     return(
       <div className="app">
-        <h2 className="header" >Welcome to the seer app</h2>
-        <form onSubmit={this.submit}>
-          {/* <button>Submit</button> */}
-        </form>
-
-        <div className="blog-">
-          {this.displayArticles(this.state.articles)}
+        <h2 className="header" >Welcome to the seer app </h2>
+        <div> {/* Display Articles */}
+          <Articles/>
         </div>
+
+        
       </div>
     );
   }
