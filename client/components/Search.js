bbcbd070c94e60b2ce71dfc0a9d9f2d17fc0a68b
lsjasonchun <lsjasonchun@gmail.com>
2020-06-02 19:17:28 +1200

    Added components file and an Search.js component to populate the body of a search menu.

---
+++ client/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export class Search extends React.Component {
+    
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    };
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Articles} from './components/Articles.js';
+import {Search} from '../components/Search';
 
 class App extends React.Component {
 
@@ -9,11 +10,12 @@ class App extends React.Component {
     return(
       <div className="app">
         <h2 className="header" >Welcome to the seer app </h2>
+       
+        <h2 className="header" >Welcome to the seer app</h2>
+        <div>{Search}</div>
         <div> {/* Display Articles */}
           <Articles/>
         </div>
-
-        
       </div>
     );
   }
