7bcaeae046e5de74dd71dafa3ccf66f7a6118576
lsjasonchun <lsjasonchun@gmail.com>
2020-06-02 21:46:20 +1200

    Added tabs, description, save and search buttons, and a SE practice dropdown checkbox.

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,37 +1,45 @@
 import React from 'react';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 import '../App.css';
+
+const options = [
+    { value: 'TDD', label: 'Test Driven Development' },
+    { value: 'OOP', label: 'Object Orientated Programming' },
+    { value: 'DynP', label: 'Dynamic Programming' },
+  ];
 
 export class Search extends React.Component {
     
+
     render() {
         return(
-            <div>
+            <div class="div_border">
                 <form>
-                <div>
-                    <input placeholder="Enter search description"></input>   
-                    <button>Search</button>
-                </div>
-                <div class="tab">
-                    <button onClick="" class="tablinks">Tab 1</button>
-                    <button onClick="" class="tablinks">Tab 2</button>
-                </div> 
-    
-                <div>
+                    <div class="tab">
+                        <button onClick="" class="tablinks">Tab 1</button>
+                        <button onClick="" class="tablinks">Tab 2</button>
+                    </div> 
+
+                    <br></br>
+
+                    <label for="description">Description  </label>
+                    <input id="description" type="text" placeholder="Enter search description"></input>
+
+                    <br></br>
+                    <br></br>
+        
                     {/* Date range */}
-                    <input></input>
-                </div>
-    
-                <div id="sePractice">
-                    <span>SE Practice/Methods</span>
-                    <ul>
-                        <li><input type="checkbox"/>TDD</li>
-                        <li><input type="checkbox"/>Algorithms</li>
-                        <li><input type="checkbox"/>Object Orientated Applications</li>
-                        <li><input type="checkbox"/>Unit Tests</li>
-                    </ul>
-                </div>
-                </form>
-            </div>
-        )
+                    {/* <label for="dateRange">Date Range</label> */}
+
+                    <label>Select SE Practice(s)</label>
+                    <ReactMultiSelectCheckboxes options={options} />
+
+                    <br></br>
+
+                    <input type="submit" value="Save"></input>
+                    <input type="submit" value="Search"></input>
+                </form> 
+            </div> 
+        );
     }
 }
