33c54b8e57cf780a7d731b1e49a8714daf068815
temaari <53286793+temaari@users.noreply.github.com>
2020-06-05 05:31:11 +1200

    possible changes for search Feature

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,14 +1,45 @@
 import React from 'react';
+import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
 import axios from 'axios';
-import {Search} from './Search.js';
+import {Search} from './Search.js'
 import '../App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker } from 'react-dates';
+import { addDays } from 'date-fns';
+
+const options = [
+    { value: 'TDD', label: 'Test Driven Development' },
+    { value: 'OOP', label: 'Object Orientated Programming' },
+    { value: 'DynP', label: 'Dynamic Programming' },
+];
 
 export class Articles extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state ={
+            startDate : null,
+            endDate: null
+        }
+    }
+
     state = {
+        description: "",
+        selectedOptionSE: "",
         articles: []
-    };
-      
+    }
+
+
+    // alertStartDate = () => {
+    //     alert(this.state.startDate);
+    // }
+    // alertEndDate = () => {
+    //     alert(this.state.endDate);
+    // }
+
     componentDidMount = () => {
         this.getArticles();
     };
@@ -26,24 +57,82 @@ export class Articles extends React.Component {
             });
     };
 
+    handleSelectedSE = (event) => {
+        // this.setState(
+        //     { selectedOptionSE },
+        //     () => console.log(`Option selected:`, this.state.selectedOptionSE)
+        // )
+        // console.log(event.target);
+    }
 
-    displayArticles = (articles) => {
-        if (!articles.length) return null;
-
-        return articles.map((article, index) => (
-            <div key={index} className="articles_display">
-                <h3 className="">{article.author}</h3>
-                <p>{article.title}</p>
-            </div>
-        ));
-    };
+    handleDescriptionInput = (e) => {
+        this.setState({
+            description: e.target.value,
+        })
+        console.log(e.target.value);
+    }
 
     render() {
+        
         return(
-            <div className="blog-">
-                <Search/>
-                {this.displayArticles(this.state.articles)}
-            </div>
-        )
+            <div className="div_border">
+                <form>
+                    <div className="tab">
+                        <button className="tablinks">Tab 1</button>
+                        <button className="tablinks">Tab 2</button>
+                    </div> 
+
+                    <br></br>
+
+                    <label id="description">Description  </label>
+                    <input id="description" type="text" onChange={this.handleDescriptionInput} placeholder="Enter search description"></input>
+
+                    <br></br>
+                    <br></br>
+
+                    <label for="date range">Choose Date Range -</label>
+                    {/* <br/> */}
+                    <DateRangePicker
+                        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                        startDatePlaceholderText="from"
+                        endDatePlaceholderText="to"
+                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                        /* numberOfMonths={()=> 1}  */
+                        isOutsideRange={()=> false} //show previous dates as well 
+                        maximumDate={addDays(new Date(), 0)} //should only allow "to" is current date and not future dates
+                        showClearDates={true} //allow us to clear dates
+                        
+                        
+                    />
+                    {/* Date range */}
+                    {/* <label for="dateRange">Date Range</label> */}
+
+                    {/* this is for the buttons to show the dates chosen */}
+                    {/* <button onCLick={this.alertStartDate}>Click Me for Start Date</button> 
+                    <button onCLick={this.alertEndDate}>Click Me for Start Date</button> */}
+
+                    <br></br>
+                    <label>Select SE Practice(s)</label>
+                    <ReactMultiSelectCheckboxes 
+                        onChange={this.handleSelectedSE}
+                        options={options} 
+                    />
+
+                    <br></br>
+                    <br></br>
+
+                    <input type="submit" value="Save"></input>
+                    <input type="submit" value="Search"></input>
+                </form> 
+                <div>
+                    <Search articles={this.state.articles} term={this.state.description}/>
+                </div>
+            </div> 
+        );
     }
 }
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,118 +1,39 @@
-import React, { Component } from 'react';
-import ReactMultiSelectCheckboxes from 'react-multiselect-checkboxes';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker } from 'react-dates';
-import { addDays } from 'date-fns';
-
-
+import React from 'react';
 import '../App.css';
-
-
-const options = [
-    { value: 'TDD', label: 'Test Driven Development' },
-    { value: 'OOP', label: 'Object Orientated Programming' },
-    { value: 'DynP', label: 'Dynamic Programming' },
-  ];
+import {ArticleCard} from './ArticleCard.js';
 
 export class Search extends React.Component {
+    
+    displayArticles = (articles) => {
+        if (!articles.length) return null;
 
-    state = {
-        description: "",
-        selectedOptionSE: ""
-    }
-
-    handleSelectedSE = selectedOptionSE => {
-        this.setState(
-            { selectedOptionSE },
-            () => console.log(`Option selected:`, this.state.selectedOptionSE)
+        let filterArticles = articles.filter(searchingFor(this.props.term)).map((article, i) => (
+            <ArticleCard 
+                index={i}
+                title={article.title}
+                author={article.author}
+                month={article.month}
+                year={article.year}
+                annote={article.annote}
+            />
+        ));
+        return filterArticles;
+    };
+    
+    render() {
+        return(
+            <div className="blog-">
+                {this.displayArticles(this.props.articles)}
+            </div>
         )
     }
+}
 
-    handleDescriptionInput = (e) => {
-        this.setState({
-            description: e.target.value
-        })
-    }
-    constructor(props) {
-        super(props);
-        this.state ={
-            startDate : null,
-            endDate: null
+function searchingFor(term) {
+    return function (x) {
+        if (term !== null) {
+        return (x.title.toLowerCase().includes(term.toLowerCase())
+            || !term)
         }
-    }
-
-    // alertStartDate = () => {
-    //     alert(this.state.startDate);
-    // }
-    // alertEndDate = () => {
-    //     alert(this.state.endDate);
-    // }
-
-    render() {
-        const { description } = this.state;
-        const { selectedOptionSE } = this.state;
-        
-        return(
-            <div class="div_border">
-                <form>
-                    <div class="tab">
-                        <button onClick="" class="tablinks">Tab 1</button>
-                        <button onClick="" class="tablinks">Tab 2</button>
-                    </div> 
-
-                    <br></br>
-
-                    <label for="description">Description  </label>
-                    <input id="description" type="text" onChange="this.handleDescriptionInput()" placeholder="Enter search description"></input>
-
-                    <br></br>
-                    <br></br>
-
-                    <label for="date range">Choose Date Range -</label>
-                    {/* <br/> */}
-                    <DateRangePicker
-                        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-                        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-                        startDatePlaceholderText="from"
-                        endDatePlaceholderText="to"
-                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-                        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-                        /* numberOfMonths={()=> 1}  */
-                        isOutsideRange={()=> false} //show previous dates as well 
-                        maximumDate={addDays(new Date(), 0)} //should only allow "to" is current date and not future dates
-                        showClearDates={true} //allow us to clear dates
-                        
-                        
-                    />
-                    {/* Date range */}
-                    {/* <label for="dateRange">Date Range</label> */}
-
-                    {/* this is for the buttons to show the dates chosen */}
-                    {/* <button onCLick={this.alertStartDate}>Click Me for Start Date</button> 
-                    <button onCLick={this.alertEndDate}>Click Me for Start Date</button> */}
-
-                    <br/>
-                    <br/>
-                    <label>Select SE Practice(s)</label>
-                    <ReactMultiSelectCheckboxes 
-                        value={selectedOptionSE}
-                        onChange={this.handleSelectedSE}
-                        options={options} 
-                    />
-
-                    <br></br>
-                    <br></br>
-
-                    <input type="submit" onChange="" value="Save"></input>
-                    <input type="submit" onClick="this.getSelectedConstraints()" value="Search"></input>
-                </form> 
-            </div> 
-        );
-    }
+    }   
 }
